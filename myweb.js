8f3b95838c498f3778499cb21ecc1de03b568d35
Rajesh Rathor <rathorrajesh906@gmail.com>
2023-05-16 18:15:08 +0530

    master

---
+++ myweb.js
@@ -0,0 +1,16 @@
+let items = document.getElementsByClassName("items")[0];
+let animation = document.getElementsByClassName("animation")[0];
+// console.log(animatio/);
+
+// items.addEventListener("click", () => {
+//   if (animation.style.width == "1%") {
+//     animation.style.width = "100%";
+//   } else {
+//     animation.style.width = "1%";
+//   }
+// });
+// const idanimation = document.getElementById("idanimation");
+// items.addEventListener("click", () => {
+//   idanimation.classList.toggle("animation");
+// });
+async function red() {}
